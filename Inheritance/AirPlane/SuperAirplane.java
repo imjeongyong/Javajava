dd02665d237b852b19b0fc2766b00cf820b5ba9d
KJY <KJY@DESKTOP-FMFUT7B>
2022-11-19 17:02:55 +0900

    supur.의 사용

---
+++ Inheritance/AirPlane/SuperAirplane.java
@@ -0,0 +1,16 @@
+package AirPlane;
+
+public class SuperAirplane extends Airplane {
+	public static final int NORMAL = 1;
+	public static final int SUPER = 2;
+
+	public int flyMode = NORMAL;
+
+	public void fly() {
+		if (flyMode == SUPER) {
+			System.out.println("초음속비행중...");
+		} else {
+			super.fly(); // 부모클래스의 fly메서드 호출!
+		}
+	}
+}
