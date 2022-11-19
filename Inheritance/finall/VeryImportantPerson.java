f795eab7ff4d3c17bc11902eb151399521cc2e0e
KJY <KJY@DESKTOP-FMFUT7B>
2022-11-19 17:10:00 +0900

    final class는 상속 불가

---
+++ Inheritance/finall/VeryImportantPerson.java
@@ -0,0 +1,6 @@
+package finall;
+
+public class VeryImportantPerson { // extends Member <- Member라는 클래스를 상속하면 
+	                               // The type VeryImportantPerson cannot subclass the final class Member
+								   // 오류 발생!!
+}
