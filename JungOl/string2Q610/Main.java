98639bcf5b8ed969dd8de66cd12e8f62c5ff9448
KJY <KJY@DESKTOP-FMFUT7B>
2023-01-10 09:28:31 +0900

    230110

---
+++ JungOl/string2Q610/Main.java
@@ -0,0 +1,29 @@
+package string2Q610;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String[] strAr = new String[5];
+		for (int i = 0; i < strAr.length; i++) {
+			strAr[i] = sc.next();
+		}
+		sc.close();
+
+		for (int i = 0; i < strAr.length; i++) {
+			for (int j = 0; j < strAr.length - 1; j++) {
+				if (strAr[j].compareTo(strAr[j + 1]) < 0) {
+					String tmp = strAr[j];
+					strAr[j] = strAr[j + 1];
+					strAr[j + 1] = tmp;
+				}
+			}
+		}
+
+		for (int i = 0; i < strAr.length; i++) {
+			System.out.println(strAr[i]);
+		}
+	}
+}
